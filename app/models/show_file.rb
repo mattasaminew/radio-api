6f84847c975ff8d85c9abcacbaeb7ffaed92bba5
Matthew Asaminew <mattasaminew@gmail.com>
2018-04-16 15:56:17 -0700

    Update showfile scope

--- app/models/show_file.rb
+++ app/models/show_file.rb
@@ -1,5 +1,5 @@
 class ShowFile < ApplicationRecord
 	self.table_name = "cc_files"
 
-	scope :select_from_show_name, ->(show_name) { where(hidden: false, artist_name: show_name) } 
+	scope :select_from_show_name, ->(show_name) { where(file_exists: true, hidden: false, artist_name: show_name) } 
 end
