13e4f56c93f7a8d518dfa7fb216fea15daf97297
Matthew Asaminew <mattasaminew@gmail.com>
2018-04-17 02:39:49 -0700

    Add cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+	before_action :set_cors_headers
+
+	private
+
+	def set_cors_headers
+		if allowed_origins.include?(request.headers['Origin'])
+			headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+			headers['Access-Control-Request-Method'] = 'GET'
+			headers['Access-Control-Allow-Headers'] = '*'
+			Rails.logger.info "[ApplicationController] Access-Control-Allow-Origin: #{request.headers['Origin']}"
+		else
+			Rails.logger.info "[ApplicationController] No Access-Control-Allow-Origin set (#{request.headers['Origin']} not in allowed_origins)"
+		end
+	end
+
+	def allowed_origins
+		['http://localhost:4000']
+	end
 end
